abb48e52a261739541794fe056ed8b66999c07dc
TPG4155 <33572873+TPG4155@users.noreply.github.com>
2017-11-11 14:08:25 +0100

    Add files via upload

---
+++ Codes/addBoundry.m
@@ -0,0 +1,17 @@
+function [M,B] = addBoundry(M)
+B = zeros(size(M));
+
+top = 1/4;
+bottom = 1;
+left = 1;
+right = 1;
+
+B(:,1) = left;
+B(1,2:end-1) = top;
+B(end,2:end-1) = bottom;
+B(:,end) = right;
+M(:,1) = 0;
+M(1,2:end-1) = 0;
+M(end,2:end-1) = 0;
+M(:,end) = 0;
+end
